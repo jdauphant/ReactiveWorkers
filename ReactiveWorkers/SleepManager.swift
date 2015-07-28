06220ab356969ae702136a95969d5a3d34bdbc33
Julien DAUPHANT <judauphant@gmail.com>
2015-07-28 17:45:43 +0200

    Generate a async test for sleep

--- ReactiveWorkers/SleepManager.swift
+++ ReactiveWorkers/SleepManager.swift
@@ -22,16 +22,21 @@ public class SleepManager: TaskPool {
         func start() -> SignalProducer<TaskResult, NSError> {
             return SignalProducer {
                 sink, disposable in
-                sendNext(sink, SleepTaskResult(message:"Start to sleep \(self.id)"))
-                NSThread.sleepForTimeInterval(self.timeInterval)
-                sendNext(sink, SleepTaskResult(message:"Good Sleep \(self.id)"))
-                sendCompleted(sink)
+                
+                sendNext(sink, SleepTaskResult(message:"Start to sleep #\(self.id)"))
+                
+                let delayTime = dispatch_time(DISPATCH_TIME_NOW,
+                    Int64(self.timeInterval * Double(NSEC_PER_SEC)))
+                dispatch_after(delayTime, dispatch_get_main_queue()) {
+                    sendNext(sink, SleepTaskResult(message:"Good Sleep #\(self.id)"))
+                    sendCompleted(sink)
+                }
             }
         }
     }
     
-    public func sleep(timeInterval: Double = 2, id: Int = Int(rand())) -> SignalProducer<String, NSError> {
-        return add(SleepTask(timeInterval: timeInterval, id: id))
+    public func sleep(timeInterval: Double = 2, id: Int = Int(rand()), priority: Priority = .Medium) -> SignalProducer<String, NSError> {
+        return add(SleepTask(timeInterval: timeInterval, id: id), priority: priority)
             |> map { result in (result as! SleepTaskResult).message }
     }
 }
